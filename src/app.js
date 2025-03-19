325cb85953a9d2f0cbbe701967b6c6a3ddeed92c
Akshat Bandooni <63807929+Akshatbandooni-rgb@users.noreply.github.com>
2025-03-19 21:56:50 +0530

    Add middleware for CORS, cookie parsing, logging, and JSON handling

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const morgan = require("morgan");
 
 // Load environment variables
 if (dotenv.config().error) {
@@ -11,6 +14,13 @@ if (dotenv.config().error) {
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//Middlewares
+app.use(cors()); // Enable CORS for all routes
+app.use(cookieParser()); // Parse cookies in incoming requests
+app.use(morgan("dev")); // Log HTTP requests in the console
+app.use(express.json()); // Parse JSON payloads
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded payloads
 
 // Connect to the database and start the server
 connectDB()
