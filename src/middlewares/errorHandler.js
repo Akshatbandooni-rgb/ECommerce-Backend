c9963326e1f021214841ff22f746699954f2948a
Akshat Bandooni <63807929+Akshatbandooni-rgb@users.noreply.github.com>
2025-03-22 01:39:13 +0530

    Refactor error handler and add user registration validation

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -4,9 +4,7 @@ const errorHandler = (error, req, res, next) => {
   if (error instanceof APIError) {
     return res.status(error.statusCode).json(error.toJSON());
   }
-  const err = new APIError.internalServerError(
-    ReasonPhrases.INTERNAL_SERVER_ERROR
-  );
+  const err = APIError.internalServerError(ReasonPhrases.INTERNAL_SERVER_ERROR);
   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err.toJSON());
 };
 module.exports = errorHandler;
