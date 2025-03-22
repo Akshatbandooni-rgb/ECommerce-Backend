2c20d7a9269057ff7a89c11ead20b07f886640a9
Akshat Bandooni <63807929+Akshatbandooni-rgb@users.noreply.github.com>
2025-03-23 02:08:30 +0530

    feat(constants): Add cookie token name and user roles to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,9 @@
 const Constants = {
-    DB_NAME:"ECommerce",
-}
-module.exports = Constants
+  DB_NAME: "ECommerce",
+  COKKIE_TOKEN_NAME: "auth_token",
+  USER_ROLES: {
+    ADMIN: "admin",
+    USER: "user",
+  },
+};
+module.exports = Constants;
