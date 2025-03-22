7d10f778a87781f09599820096b6ae1b1c1cfb82
Akshat Bandooni <63807929+Akshatbandooni-rgb@users.noreply.github.com>
2025-03-22 12:52:13 +0530

    Enhance user registration by adding email existence check and improving validation

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,19 +1,27 @@
+const { StatusCodes } = require("http-status-codes");
 const User = require("../models/user-model");
 const validateRegisterUser = require("../validators/registerUser-validation");
 const APIResponse = require("../utils/APIResponse");
-const { StatusCodes } = require("http-status-codes");
+const APIError = require("../utils/APIError");
+
 const registerUser = async (req, res, next) => {
   try {
+    // Validate user input
     validateRegisterUser(req.body);
+
     const { firstName, lastName, email, password } = req.body;
-    const UserData = {
-      firstName,
-      lastName,
-      email,
-      password,
-    };
-    const user = new User(UserData);
+
+    // Check if the user already exists
+    const isUserExist = await User.findOne({ email });
+    if (isUserExist) {
+      throw APIError.badRequest("Email is already registered");
+    }
+
+    // Create a new user
+    const user = new User({ firstName, lastName, email, password });
     await user.save();
+
+    // Send success response
     const successResponse = new APIResponse(
       "User registered successfully",
       StatusCodes.CREATED
