a8aa751432d6b31f6a2e93f2e96c43b64c9f05f0
Akshat Bandooni <63807929+Akshatbandooni-rgb@users.noreply.github.com>
2025-03-31 23:03:30 +0530

    feat(validation): Enhance profile edit validation to return invalid fields

--- src/validators/userValidation.js
+++ src/validators/userValidation.js
@@ -1,7 +1,13 @@
 const ALLOWED_UPDATE_FIELDS = new Set(["firstName", "lastName"]);
 
 const validateProfileEditData = (data) => {
-  return Object.keys(data).every((key) => ALLOWED_UPDATE_FIELDS.has(key));
+  const keys = Object.keys(data);
+  const invalidFields = keys.filter((key) => !ALLOWED_UPDATE_FIELDS.has(key));
+
+  return {
+    isValidUpdate: invalidFields.length === 0,
+    invalidFields: invalidFields,
+  };
 };
 
 module.exports = { validateProfileEditData };
